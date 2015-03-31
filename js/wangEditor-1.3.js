fa353c3ec21f996ea6d572acd6dcd513c1e94415
王福朋 <wangfupeng1988@163.com>
2015-03-31 21:19:06 +0800

    处理中文图片名

--- js/wangEditor-1.3.js
+++ js/wangEditor-1.3.js
@@ -1445,39 +1445,42 @@
         //command
         'command': function(e, commandName, commandValue, callback){
             if( !this.currentRange() ){
-                if(e){
-                    e.preventDefault();
-                }
-                return;
-            }
-
-            var commandHook;
-
-            //恢复选中区
-            this.restoreSelection();
-
-            //执行
-            if($E.commandEnabled(commandName) === true){
-                document.execCommand(commandName, false, commandValue);
+                alert('未选中编辑区，无法执行操作');
             }else{
-                commandHook = this.commandHooks[commandName];
-                if(commandHook){
-                    commandHook.call(this, commandName, commandValue);
+                var commandHook;
+
+                //恢复选中区
+                this.restoreSelection();
+
+                //执行
+                if($E.commandEnabled(commandName) === true){
+                    document.execCommand(commandName, false, commandValue);
                 }else{
-                    $E.consoleLog('不支持“' + commandName + '”命令，请检查');
+                    commandHook = this.commandHooks[commandName];
+                    if(commandHook){
+                        commandHook.call(this, commandName, commandValue);
+                    }else{
+                        $E.consoleLog('不支持“' + commandName + '”命令，请检查');
+                    }
                 }
-            }
 
-            //重新保存，否则chrome，360，safari，opera中会清空currentRange
-            this.saveSelection();
-            
-            //执行回调函数
-            if(callback && typeof callback === 'function'){
-                callback(this);
-            }
+                //重新保存，否则chrome，360，safari，opera中会清空currentRange
+                this.saveSelection();
+                
+                //执行回调函数
+                if(callback && typeof callback === 'function'){
+                    callback(this);
+                }
 
-            //更新菜单样式
-            this.updateMenuStyle();
+                //更新菜单样式
+                this.updateMenuStyle();
+
+                //记录，以便撤销
+                this.addCommandRecord();
+
+                //变化监控
+                this.change();
+            }
 
             //关闭modal
             this.$modalContainer.find('.wangEditor-modal:visible').hide();
@@ -1485,15 +1488,9 @@
                 $maskDiv.hide();  //关闭遮罩层
             }
 
-            //记录，以便撤销
-            this.addCommandRecord();
-
             if(e){
                 e.preventDefault();
             }
-
-            //变化监控
-            this.change();
         }
     });
 
