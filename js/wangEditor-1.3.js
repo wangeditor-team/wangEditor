24e97047cb261a779cd4522dc1c5fc404a5f7e1f
王福朋 <wangfupeng1988@163.com>
2015-05-09 13:52:12 +0800

    增加插入视频的功能

--- js/wangEditor-1.3.js
+++ js/wangEditor-1.3.js
@@ -61,7 +61,7 @@
                 'icon-wangEditor-cw':'&#xe81a;',
                 'icon-wangEditor-ccw':'&#xe81b;',
                 'icon-wangEditor-music':'&#xe911;',
-                'icon-wangEditor-play':'&#xe192;',
+                'icon-wangEditor-play':'&#xe912;',
                 'icon-wangEditor-location':'&#xe947;',
                 'icon-wangEditor-happy':'&#xe9df;'
             };
@@ -232,7 +232,9 @@
             //dropPanel-big
             'dropPanelBig': '<ul class="wangEditor-drop-panel wangEditor-drop-panel-big">{content}</ul>',
             //dropPanel-floatItem
-            'dropPanel_floatItem': '<div class="wangEditor-drop-panel-floatItem">{content}</div>'
+            'dropPanel_floatItem': '<div class="wangEditor-drop-panel-floatItem">{content}</div>',
+            //视频
+            'videoEmbed': '<embed src="{src}" allowFullScreen="true" quality="high" width="{width}" height="{height}" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>'
         },
         //表情配置（1.gif, 2.gif, 3.gif ... 100.gif）
         'expressionConfig': {
@@ -1266,6 +1268,67 @@
                         return $panel; 
                     }
                 },
+                'insertVideo': {
+                    'title': '插入视频',
+                    'type': 'modal',
+                    'txt': 'icon-wangEditor-play',
+                    'modal': function(editor){
+                        var txtSrcId = $E.getUniqeId(),
+                            txtWidthId = $E.getUniqeId(),
+                            txtHeightId = $E.getUniqeId(),
+                            btnId = $E.getUniqeId(),
+                            defaultWidth = 480, defaultHeight = 400;
+                        var content = '地址：<input id="' + txtSrcId + '" type="text" style="width:300px;"/> <br>' +
+                                        '宽度：<input id="' + txtWidthId + '" type="text" style="width:50px" value="' + defaultWidth + '"/> px（像素） <br>' +
+                                        '高度：<input id="' + txtHeightId + '" type="text" style="width:50px" value="' + defaultHeight + '"/> px（像素） <br>' +
+                                        '<button id="' + btnId + '" class="wangEditor-modal-btn">插入视频</button>';
+                        var $video_modal = $(
+                                $E.htmlTemplates.modalSmall.replace('{content}', content)
+                            );
+
+                        //插入视频点击事件
+                        $video_modal.find('#' + btnId).click(function(e){
+                            var src = $.trim( $('#' + txtSrcId).val() ), 
+                                width = +( $('#' + txtWidthId).val() ),
+                                height = +( $('#' + txtHeightId).val() ),
+                                embed,
+                                video_callback = function(){
+                                    $('#' + txtSrcId).val('');
+                                    $('#' + txtWidthId).val(defaultWidth);
+                                    $('#' + txtHeightId).val(defaultHeight);
+                                }
+
+                            //验证src的合法性
+                            if($E.filterXSSForUrl(src) === false){
+                                alert('您的输入内容有不安全字符，请重新输入！')
+                                return;
+                            }
+
+                            //在此验证src
+                            if( (src.indexOf('http://') !== 0 && src.indexOf('https://') !== 0) || src.indexOf('.swf') === -1 ){
+                                alert('您输入的内容不符合要求');
+                                return;
+                            }
+
+                            //验证高度和宽度的合法性，不合法则使用默认配置
+                            if(isNaN(width)){
+                                width = defaultWidth;
+                            }
+                            if(isNaN(height)){
+                                height = defaultHeight;
+                            }
+
+                            embed = $E.htmlTemplates.videoEmbed
+                                    .replace('{src}', src)
+                                    .replace('{width}', width)
+                                    .replace('{height}', height);
+
+                            editor.command(e, 'insertHTML', embed, video_callback);
+                        });
+
+                        return $video_modal;
+                    }
+                },
                 'insertHr': {
                     'title': '插入横线',
                     'type': 'btn',
@@ -1728,7 +1791,7 @@
                 ['indent', 'outdent'],
                 ['unOrderedList', 'orderedList'],
                 ['justifyLeft', 'justifyCenter', 'justifyRight'] ,
-                ['createLink', 'unLink', 'insertExpression'],
+                ['createLink', 'unLink', 'insertExpression', 'insertVideo'],
                 ['insertHr', 'insertTable', 'webImage', 'uploadImg', 'insertLocation','insertSimpleCode'],
                 ['undo', 'redo']
             ];
