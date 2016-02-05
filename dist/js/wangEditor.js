bc0aa3d2b1ce864fc46758d98cadbd99f826a64b
wangfupeng <wangfupeng1988@163.com>
2016-02-05 16:47:01 +0800

    2.0.4

--- dist/js/wangEditor.js
+++ dist/js/wangEditor.js
@@ -1003,6 +1003,8 @@ _e(function (E, $) {
         this.editor = opt.editor;
         this.id = opt.id;
         this.title = opt.title;
+        this.$domNormal = opt.$domNormal;
+        this.$domSelected = opt.$domSelected || opt.$domNormal;
 
         // document.execCommand 的参数
         this.commandName = opt.commandName;
@@ -1027,6 +1029,11 @@ _e(function (E, $) {
         var uiConfig = editor.UI.menus;
         var menuId = this.id;
         var menuUI = uiConfig[menuId];
+
+        if (this.$domNormal && this.$domSelected) {
+            // 自定义的菜单中，已经传入了 $dom 无需从配置文件中查找生成
+            return;
+        }
 
         if (menuUI == null) {
             E.warn('editor.UI配置中，没有菜单 "' + menuId + '" 的UI配置，只能取默认值');
@@ -2622,120 +2629,120 @@ _e(function (E, $) {
      E.UI.menus = {
         // 这个 default 不加引号，在 IE8 会报错
         'default': {
-            normal: '<a href="#"><i class="wangeditor-menu-img-command"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-command"></i></a>',
             selected: '.selected'
         },
         bold: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-bold"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-bold"></i></a>',
             selected: '.selected'
         },
         underline: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-underline"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-underline"></i></a>',
             selected: '.selected'
         },
         italic: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-italic"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-italic"></i></a>',
             selected: '.selected'
         },
         forecolor: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-pencil"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-pencil"></i></a>',
             selected: '.selected'
         },
         bgcolor: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-brush"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-brush"></i></a>',
             selected: '.selected'
         },
         strikethrough: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-strikethrough"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-strikethrough"></i></a>',
             selected: '.selected'
         },
         eraser: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-eraser"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-eraser"></i></a>',
             selected: '.selected'
         },
         quote: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-quotes-left"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-quotes-left"></i></a>',
             selected: '.selected'
         },
         source: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-code"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-code"></i></a>',
             selected: '.selected'
         },
         fontfamily: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-font2"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-font2"></i></a>',
             selected: '.selected'
         },
         fontsize: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-text-height"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-text-height"></i></a>',
             selected: '.selected'
         },
         head: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-header"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-header"></i></a>',
             selected: '.selected'
         },
         orderlist: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-list-numbered"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-list-numbered"></i></a>',
             selected: '.selected'
         },
         unorderlist: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-list-bullet"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-list-bullet"></i></a>',
             selected: '.selected'
         },
         alignleft: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-align-left"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-align-left"></i></a>',
             selected: '.selected'
         },
         aligncenter: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-align-center"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-align-center"></i></a>',
             selected: '.selected'
         },
         alignright: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-align-right"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-align-right"></i></a>',
             selected: '.selected'
         },
         link: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-link"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-link"></i></a>',
             selected: '.selected'
         },
         unlink: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-unlink"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-unlink"></i></a>',
             selected: '.selected'
         },
         table: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-table"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-table"></i></a>',
             selected: '.selected'
         },
         emotion: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-happy"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-happy"></i></a>',
             selected: '.selected'
         },
         img: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-picture"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-picture"></i></a>',
             selected: '.selected'
         },
         vedio: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-play"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-play"></i></a>',
             selected: '.selected'
         },
         location: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-location"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-location"></i></a>',
             selected: '.selected'
         },
         insertcode: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-terminal"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-terminal"></i></a>',
             selected: '.selected'
         },
         undo: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-ccw"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-ccw"></i></a>',
             selected: '.selected'
         },
         redo: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-cw"></i></a>',
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-cw"></i></a>',
             selected: '.selected'
         },
         fullscreen: {
-            normal: '<a href="#"><i class="wangeditor-menu-img-enlarge2"></i></a>',
-            selected: '<a href="#" class="selected"><i class="wangeditor-menu-img-shrink2"></i></a>'
+            normal: '<a href="#" tabindex="-1"><i class="wangeditor-menu-img-enlarge2"></i></a>',
+            selected: '<a href="#" tabindex="-1" class="selected"><i class="wangeditor-menu-img-shrink2"></i></a>'
         }
      };
      
