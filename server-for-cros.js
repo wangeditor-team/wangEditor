a2f2f2769394538e8083fd55b685cb6e7fbcd8fb
Arliang <arliang725@163.com>
2016-03-30 11:44:19 +0800

    使用path.join来拼接路径，删除server-windows.js

--- server-for-cros.js
+++ server-for-cros.js
@@ -13,7 +13,7 @@ var path = require('path');
 
 // 文件将要上传到哪个文件夹下面
 var uploadfoldername = 'uploadfiles';
-var uploadfolderpath = __dirname + '/' + uploadfoldername;
+var uploadfolderpath = path.join(__dirname, uploadfoldername);
 
 // var server = '192.168.1.2';
 var server = 'localhost';
@@ -89,7 +89,7 @@ http.createServer(function (req, res) {
                 filename = Math.random().toString().slice(2) + extname;
 
                 // 构建将要存储的文件的路径
-                var filenewpath = uploadfolderpath + '/' + filename;
+                var filenewpath = path.join(uploadfolderpath, filename);
 
                 // 将临时文件保存为正式的文件
                 fs.rename(tempfilepath, filenewpath, function (err) {
