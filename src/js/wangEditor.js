c251ac92e5497c2a2d955149960bff11a742171e
陆文杰 <luwenjiechn@qq.com>
2017-05-22 17:10:04 +0800

    Update wangEditor.js
    修复页面存在多个编辑器时，上传图片总会放到最后一个编辑器的问题

--- src/js/wangEditor.js
+++ src/js/wangEditor.js
@@ -3547,8 +3547,8 @@ _e(function (E, $) {
 
     E.fn.initDefaultConfig = function () {
         var editor = this;
-        editor.config = $.extend({}, E.config);
-        editor.UI = $.extend({}, E.UI);
+        editor.config = $.extend(true, {}, E.config);
+        editor.UI = $.extend(true, {}, E.UI);
     };
 
 });
