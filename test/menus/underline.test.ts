a9a3aa22b0aaec4fbb7fd1f38313d2f353314b81
NeverSellSuteAC <1196417540@qq.com>
2020-06-18 22:30:22 +0800

    test:修改测试内容中的部分描述和名称

--- test/menus/underline.test.ts
+++ test/menus/underline.test.ts
@@ -12,13 +12,13 @@ import { getMenuInstance } from '../fns/menus'
 let editor: Editor
 let underlineMenu: Underline
 
-test('加粗', () => {
+test('下划线', () => {
     editor = createEditor(document, 'div1') // 赋值给全局变量
 
-    // 找到 bold 菜单
+    // 找到 underline 菜单
     underlineMenu = getMenuInstance(editor, Underline) as Underline
 
-    // 执行点击事件，模拟加粗
+    // 执行点击事件，模拟下划线功能
     mockCmdFn(document)
         ; (underlineMenu as Underline).clickHandler()
     expect(document.execCommand).toBeCalledWith('underline', false, undefined) // mock fn 被调用
