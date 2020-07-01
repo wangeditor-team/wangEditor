46eec4e454a350c95cdeb5a3c1eb0550fe50c3b8
wangfupeng <wangfupeng1988@163.com>
2020-07-01 17:01:31 +0800

    feat: 增加 server

---
+++ server/router.js
@@ -0,0 +1,22 @@
+/**
+ * @description 服务端路由
+ * @author wangfupeng
+ */
+
+const router = require('koa-router')()
+const saveFiles = require('./controller/save-file')
+
+router.prefix('/')
+
+// 测试用
+router.get('/test', async function (ctx, next) {
+    ctx.body = 'hello test'
+})
+
+// 上传图片
+router.post('/upload-img', async function (ctx, next) {
+    const data = await saveFiles(ctx.request)
+    ctx.body = data
+})
+
+module.exports = router
