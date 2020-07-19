b291ae9f4758b63502f4717fa2dacdc69bf39d93
q378532364 <378532364@qq.com>
2020-07-19 15:42:08 +0800

    feat:增加插入表情

--- src/config/menus.ts
+++ src/config/menus.ts
@@ -3,6 +3,32 @@
  * @author wangfupeng
  */
 
+import { type } from 'jquery'
+
+/*表情菜单数据结构类型*/
+/**
+ * emotions 数组结构
+ */
+export type EmotionsType = {
+    title: string
+    type: string
+    content: Array<{}>
+}
+/**
+ * type为image数组结构
+ */
+export type EmotionsContentType = {
+    alt?: string
+    src?: ''
+}
+/**
+ * click事件事件对象
+ */
+export type EventsFnType = {
+    target?: object
+}
+/*表情菜单数据结构类型END*/
+
 export default {
     menus: [
         'bold',
@@ -17,6 +43,7 @@ export default {
         'quote',
         'backColor',
         'fontColor',
+        'emoticon',
         'image',
         'video',
     ],
@@ -43,5 +70,97 @@ export default {
         '#46acc8',
         '#f9963b',
         '#ffffff',
+    ],
+    /**
+     * 表情配置菜单
+     * 数据结构{title:'',type::'',content:[{alt:'',src:''}]}
+     * 如果为emoji表情直接作为元素插入
+     */
+
+    emotions: [
+        {
+            // tab 的标题
+            title: '默认',
+            // type -> 'emoji' / 'image'
+            type: 'image',
+            // content -> 数组
+            content: [
+                {
+                    alt: '[坏笑]',
+                    src:
+                        'http://img.t.sinajs.cn/t4/appstyle/expression/ext/normal/50/pcmoren_huaixiao_org.png',
+                },
+                {
+                    alt: '[舔屏]',
+                    src:
+                        'http://img.t.sinajs.cn/t4/appstyle/expression/ext/normal/40/pcmoren_tian_org.png',
+                },
+                {
+                    alt: '[污]',
+                    src:
+                        'http://img.t.sinajs.cn/t4/appstyle/expression/ext/normal/3c/pcmoren_wu_org.png',
+                },
+            ],
+        },
+        {
+            // tab 的标题
+            title: '新浪',
+            // type -> 'emoji' / 'image'
+            type: 'image',
+            // content -> 数组
+            content: [
+                {
+                    src:
+                        'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/7a/shenshou_thumb.gif',
+                    alt: '[草泥马]',
+                },
+                {
+                    src:
+                        'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/60/horse2_thumb.gif',
+                    alt: '[神马]',
+                },
+                {
+                    src:
+                        'http://img.t.sinajs.cn/t35/style/images/common/face/ext/normal/bc/fuyun_thumb.gif',
+                    alt: '[浮云]',
+                },
+            ],
+        },
+        {
+            // tab 的标题
+            title: 'emoji',
+            // type -> 'emoji' / 'image'
+            type: 'emoji',
+            // content -> 数组
+            content: '😀 😃 😄 😁 😆 😅 😂 😊 😇 🙂 🙃 😉 😓 😪 😴 🙄 🤔 😬 🤐'.split(/\s/),
+        },
+        {
+            // tab 的标题
+            title: '手势',
+            // type -> 'emoji' / 'image'
+            type: 'emoji',
+            // content -> 数组
+            content: [
+                '🙌',
+                '👏',
+                '👋',
+                '👍',
+                '👎',
+                '👊',
+                '✊',
+                '️👌',
+                '✋',
+                '👐',
+                '💪',
+                '🙏',
+                '️👆',
+                '👇',
+                '👈',
+                '👉',
+                '🖕',
+                '🖐',
+                '🤘',
+            ],
+        },
     ],
 }
