2b72c6d0bf4059bac6bf181d9667430329419008
童汉 <798008773@qq.com>
2020-07-30 23:16:45 +0800

    feat: 添加 获取 元素段落方法 和 获取当前选取范围内的段落 方法

---
+++ src/utils/selectionn-range-top-nodes.ts
@@ -0,0 +1,84 @@
+/**
+ * @description 选取范围所有顶级(段落)节点
+ * @author tonghan
+ */
+
+import $, { DomElement } from './dom-core'
+import Editor from '../editor/index'
+import { getNodeTop } from './util'
+
+// 构造函数
+class SelectionRangeTopNodes {
+    // 定义属性
+    editor: Editor
+    $nodeList: DomElement[]
+    $startElem: DomElement | undefined
+    $endElem: DomElement | undefined
+
+    /**
+     * 构造函数
+     * @param editor
+     */
+    constructor(editor: Editor) {
+        // 初始化属性
+        this.editor = editor
+        this.$nodeList = []
+        this.$startElem = getNodeTop($(editor.selection.getSelectionStartElem()), this.editor)
+        this.$endElem = getNodeTop($(editor.selection.getSelectionEndElem()), this.editor)
+    }
+
+    /**
+     * 初始化
+     */
+    init(): void {
+        this.recordSelectionNodes($(this.$startElem))
+    }
+
+    /**
+     * 添加 节点 到nodeList
+     * @param $node 节点
+     */
+    addNodeList($node: DomElement | HTMLElement): void {
+        this.$nodeList.push($($node))
+    }
+
+    /**
+     * 是否是 选区结束 节点
+     * @param $node 节点
+     */
+    isEndElem($node: DomElement): boolean | undefined {
+        return this.$endElem?.equal($node)
+    }
+
+    /**
+     * 获取当前节点的下一个兄弟节点
+     * @param $node 节点
+     */
+    getNextSibling($node: DomElement): DomElement {
+        return $($node.elems[0].nextSibling)
+    }
+
+    /**
+     * 记录节点 - 从选区开始节点开始 一直到匹配到选区结束节点为止
+     * @param $node 节点
+     */
+    recordSelectionNodes($node: DomElement): void {
+        const $elem = getNodeTop($node, this.editor)
+        this.addNodeList($elem)
+        if (!this.isEndElem($elem)) {
+            this.recordSelectionNodes(this.getNextSibling($elem))
+        }
+    }
+
+    /**
+     * 获取 选中节点列表
+     */
+    getSelectionNodes(): DomElement[] {
+        return this.$nodeList
+    }
+}
+
+/**
+ * 导出
+ */
+export default SelectionRangeTopNodes
