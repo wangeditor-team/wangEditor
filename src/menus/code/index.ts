565da86317d9c753573053e655fcf00ffb28c710
lkw199711 <lkw199711@163.com>
2020-07-31 10:31:45 +0800

    test:合并dev分支

--- src/menus/code/index.ts
+++ src/menus/code/index.ts
@@ -7,60 +7,78 @@ import editor from '../../editor/index'
 import { PanelConf } from '../menu-constructors/Panel'
 import { getRandom } from '../../utils/util'
 import $ from '../../utils/dom-core'
+import Panel from '../menu-constructors/Panel'
+import Editor from '../../editor/index'
+import PanelMenu from '../menu-constructors/PanelMenu'
+import { MenuActive } from '../menu-constructors/Menu'
+import DropListMenu from '../menu-constructors/DropListMenu'
 
-export default function (editor: editor, video: string): PanelConf {
-    // panel 中需要用到的id
-    const inputIFrameId = getRandom('input-iframe')
-    const btnOkId = getRandom('btn-ok')
+class Code extends PanelMenu implements MenuActive {
+    constructor(editor: Editor) {
+        const $elem = $(
+            `<div class="w-e-menu">
+                <i class="w-e-icon-terminal"></i>
+            </div>`
+        )
+
+        super($elem, editor)
+    }
 
     /**
-     * 插入链接
-     * @param iframe html标签
+     * 执行命令
+     * @param value value
      */
-    function insertVideo(video: string): void {
-        editor.cmd.do('insertHTML', video + '<p><br></p>')
+    public command(value: string): void {
+        const editor = this.editor
+        editor.cmd.do('foreColor', value)
     }
 
-    const conf = {
-        width: 300,
-        height: 0,
+    /**
+     * 菜单点击事件
+     */
+    public clickHandler(): void {
+        const editor = this.editor
+        let $videoElem
 
-        // panel 中可包含多个 tab
-        tabs: [
-            {
-                // tab 的标题
-                title: '插入代码',
-                // 模板
-                tpl: `<div>
-                        <input id="${inputIFrameId}" type="text" class="block" placeholder="格式如：<iframe src=... ></iframe>"/></td>
+        // 弹出 panel
+        this.createPanel('')
+    }
+
+    /**
+     * 创建 panel
+     * @param link 链接
+     */
+    private createPanel(iframe: string): void {
+        //创建panel
+        const panel = new Panel(this, {
+            width: 300,
+            height: 200,
+            tabs: [
+                {
+                    title: '插入代码',
+                    // 模板
+                    tpl: `<div>
+                        <textarea type="text" class="block" placeholder="格式如：<iframe src=... ></iframe>"/></textarea>
                         <div class="w-e-button-container">
-                            <button id="${btnOkId}" class="right">插入</button>
+                            <button id="" class="right">插入</button>
                         </div>
                     </div>`,
-                // 事件绑定
-                events: [
-                    // 插入视频
-                    {
-                        selector: '#' + btnOkId,
-                        type: 'click',
-                        fn: () => {
-                            // 执行插入视频
-                            const $video = $('#' + inputIFrameId)
-                            let video = $video.val().trim()
-
-                            // 视频为空，则不插入
-                            if (!video) return
-
-                            insertVideo(video)
-
-                            // 返回 true，表示该事件执行完之后，panel 要关闭。否则 panel 不会关闭
-                            return true
-                        },
-                    },
-                ],
-            }, // tab end
-        ], // tabs end
+                    // 事件绑定
+                    events: [
+                        // 插入视频
+                        {},
+                    ],
+                },
+            ],
+        })
+        panel.create()
+        this.setPanel(panel)
     }
 
-    return conf
+    /**
+     * 尝试修改菜单激活状态
+     */
+    public tryChangeActive(): void {}
 }
+
+export default Code
