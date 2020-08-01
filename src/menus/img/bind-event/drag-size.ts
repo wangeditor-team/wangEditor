39fb766392f59177c0e680f967345ec0772b64f5
xiaokyo <904656778@qq.com>
2020-08-01 09:41:48 +0800

    feat:初步实现拖拽功能

--- src/menus/img/bind-event/drag-size.ts
+++ src/menus/img/bind-event/drag-size.ts
@@ -4,6 +4,8 @@ import $, { DomElement } from '../../../utils/dom-core'
 // style
 import './drag-size.less'
 
+let imgTarget: Node | undefined
+
 /**
  * 设置拖拽框的rect
  * @param $drag drag Dom
@@ -33,23 +35,27 @@ const setDragStyle = (
 /**
  * 设置拖拽事件
  * @param $drag 拖拽框的domElement
- * @param target 图片的target对象
+ * @param $textContainerElem 编辑器实例
  */
 const addDragListen = (
-    editor: Editor,
+    // editor: Editor,
     $drag: DomElement,
-    target: EventTarget,
-    boxRect: DOMRect
+    $textContainerElem: DomElement
 ) => {
     $drag.on('click', function (e: Event) {
         e.stopPropagation()
     })
     $drag.on('mousedown', '.w-e-img-drag-rb', (e: MouseEvent) => {
         e = e || event
+        e.stopPropagation()
+        e.preventDefault()
+
+        if (!imgTarget) return
 
         const firstX = e.clientX
         const firstY = e.clientY
-        const imgRect = $(target).getBoundingClientRect()
+        const boxRect = $textContainerElem.getBoundingClientRect()
+        const imgRect = $(imgTarget).getBoundingClientRect()
         const width = imgRect.width
         const height = imgRect.height
         const left = imgRect.left - boxRect.left
@@ -58,24 +64,33 @@ const addDragListen = (
 
         let setW = width
         let setH = height
-        document.onmousemove = function (e) {
-            e = e || event
+        document.onmousemove = function (ev: MouseEvent) {
+            ev = ev || event
+            ev.stopPropagation()
+            ev.preventDefault()
 
-            setW = width + (e.clientX - firstX)
-            setH = height + (e.clientY - firstY)
+            setW = width + (ev.clientX - firstX)
+            setH = height + (ev.clientY - firstY)
+
+            // 等比计算
             if (setW / setH != ratio) {
                 setH = setW / ratio
             }
 
-            $drag.find('.w-e-img-drag-show-size').text(`${setW}px * ${setH}px`)
+            $drag
+                .find('.w-e-img-drag-show-size')
+                .text(
+                    `${setW.toFixed(2).replace('.00', '')}px * ${setH
+                        .toFixed(2)
+                        .replace('.00', '')}px`
+                )
             setDragStyle($drag, setW, setH, left, top)
         }
 
         document.onmouseup = function () {
-            $(target).attr('width', setW + '')
-            $(target).attr('height', setH + '')
-            const newImgRect = $(target).getBoundingClientRect()
-
+            $(imgTarget).attr('width', setW + '')
+            $(imgTarget).attr('height', setH + '')
+            const newImgRect = $(imgTarget).getBoundingClientRect()
             setDragStyle(
                 $drag,
                 setW,
@@ -83,10 +98,13 @@ const addDragListen = (
                 newImgRect.left - boxRect.left,
                 newImgRect.top - boxRect.top
             )
-            document.onmousemove = null
+
+            document.onmousemove = document.onmouseup = null
         }
 
-        e.stopPropagation()
+        document.onmouseleave = function () {
+            document.onmousemove = document.onmouseup = null
+        }
     })
 }
 
@@ -94,28 +112,31 @@ const addDragListen = (
  * 生成一个图片指定大小的拖拽框
  * @param target 图片的对象
  */
-const setDragMask = (editor: Editor, $textContainerElem: DomElement, target: EventTarget) => {
+const setDragMask = (editor: Editor, $textContainerElem: DomElement): DomElement => {
     const $drag = $(
         `<div class="w-e-img-drag-mask">
             <div class="w-e-img-drag-rb"></div>
             <div class="w-e-img-drag-show-size"></div>
          </div>`
     )
-    const boxRect = $textContainerElem.getBoundingClientRect()
-    const rect = $(target).getBoundingClientRect()
 
-    $drag.find('.w-e-img-drag-show-size').text(`${rect.width}px * ${rect.height}px`)
-    $drag.attr(
-        'style',
-        `width: ${rect.width}px; 
-        height:${rect.width}px; 
-        left:${rect.x - boxRect.x}px;
-        top:${rect.y - boxRect.y}px;`
-    )
-
-    addDragListen(editor, $drag, target, boxRect)
-
+    addDragListen($drag, $textContainerElem)
+    $drag.hide()
     $textContainerElem.append($drag)
+    return $drag
+}
+
+/**
+ * 显示拖拽框并设置宽度
+ * @param $textContainerElem 编辑框实例
+ * @param $drag 拖拽框对象
+ */
+const showDarg = ($textContainerElem: DomElement, $drag: DomElement) => {
+    const boxRect = $textContainerElem.getBoundingClientRect()
+    const rect = $(imgTarget).getBoundingClientRect()
+    $drag.find('.w-e-img-drag-show-size').text(`${rect.width}px * ${rect.height}px`)
+    setDragStyle($drag, rect.width, rect.height, rect.left - boxRect.left, rect.top - boxRect.top)
+    $drag.show()
 }
 
 /**
@@ -124,15 +145,26 @@ const setDragMask = (editor: Editor, $textContainerElem: DomElement, target: Eve
  */
 const bindDragImgSize = (editor: Editor) => {
     const $textContainerElem = editor.$textContainerElem
-    $textContainerElem.on('click', 'img', function (e: Event) {
+
+    const $drag = setDragMask(editor, $textContainerElem)
+
+    // 图片点击事件
+    $textContainerElem.on('click', 'img', function (e: MouseEvent) {
         e = e || event
-        $textContainerElem.find('.w-e-img-drag-mask').remove()
-        e.target && setDragMask(editor, $textContainerElem, e.target)
         e.stopPropagation()
+
+        if (e.target) {
+            imgTarget = <Node>e.target
+            showDarg($textContainerElem, $drag)
+        }
+    })
+
+    $textContainerElem.find('.w-e-text').on('scroll', function () {
+        $textContainerElem.find('.w-e-img-drag-mask').hide()
     })
 
     document.onclick = function (e: Event) {
-        $textContainerElem.find('.w-e-img-drag-mask').remove()
+        $textContainerElem.find('.w-e-img-drag-mask').hide()
     }
 }
 
