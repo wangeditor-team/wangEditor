cd4a85f9ab277e25ad05e9ab2b902c420ce225ba
童汉 <798008773@qq.com>
2020-08-01 20:04:29 +0800

    fix: 参考 v3 列表实现

--- src/menus/list/index.ts
+++ src/menus/list/index.ts
@@ -46,27 +46,62 @@ class List extends DropListMenu implements MenuActive {
 
     public command(value: string): void {
         const editor = this.editor
-        const $selectionElem = editor.selection.getSelectionContainerElem()
+        const $textElem = editor.$textElem
+        editor.selection.restoreSelection()
 
-        // 判断 当前选区为 textElem 时
-        if ($selectionElem && editor.$textElem.equal($selectionElem)) {
-            // 当 当前选区 等于 textElem 时
-            // 代表 当前选区 可能是一个选择了一个完整的段落或者多个段落
-            const $elems = getSelectionRangeTopNodes(editor)
-            if ($elems.length > 0) {
-                $elems.forEach((item: DomElement) => {
-                    operateElement($(item), value, editor)
-                })
-            }
-        } else {
-            // 当 当前选区 不等于 textElem 时
-            // 代表 当前选区要么是一个段落，要么是段落中的一部分
-            if ($selectionElem && $selectionElem.length > 0) {
-                $selectionElem.forEach((item: DomElement) => {
-                    operateElement($(item), value, editor)
-                })
-            }
+        // 判断是否已经执行了命令
+        if (editor.cmd.queryCommandState(value)) {
+            return
         }
+
+        editor.cmd.do(value)
+
+        // 验证列表是否被包裹在 <p> 之内
+        let $selectionElem = $(editor.selection.getSelectionContainerElem())
+        if ($selectionElem.getNodeName() === 'LI') {
+            $selectionElem = $selectionElem.parent()
+        }
+
+        if (/^ol|ul$/i.test($selectionElem.getNodeName()) === false) {
+            return
+        }
+
+        if ($selectionElem.equal($textElem)) {
+            // 证明是顶级标签，没有被 <p> 包裹
+            return
+        }
+
+        const $parent = $selectionElem.parent()
+        if ($parent.equal($textElem)) {
+            // $parent 是顶级标签，不能删除
+            return
+        }
+
+        $selectionElem.insertAfter($parent)
+        $parent.remove()
+
+        // const editor = this.editor
+        // const $selectionElem = editor.selection.getSelectionContainerElem()
+
+        // // 判断 当前选区为 textElem 时
+        // if ($selectionElem && editor.$textElem.equal($selectionElem)) {
+        //     // 当 当前选区 等于 textElem 时
+        //     // 代表 当前选区 可能是一个选择了一个完整的段落或者多个段落
+        //     const $elems = getSelectionRangeTopNodes(editor)
+        //     if ($elems.length > 0) {
+        //         $elems.forEach((item: DomElement) => {
+        //             operateElement($(item), value, editor)
+        //         })
+        //     }
+        // } else {
+        //     // 当 当前选区 不等于 textElem 时
+        //     // 代表 当前选区要么是一个段落，要么是段落中的一部分
+        //     if ($selectionElem && $selectionElem.length > 0) {
+        //         $selectionElem.forEach((item: DomElement) => {
+        //             operateElement($(item), value, editor)
+        //         })
+        //     }
+        // }
 
         // 恢复选区
         editor.selection.restoreSelection()
