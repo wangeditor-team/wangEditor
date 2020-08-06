4628fbca77ab35cd4718e753acd3bce8dfafd514
lichunlin <ichun0618@163.com>
2020-08-06 14:18:15 +0800

    fix：修复默认行为被阻止

--- src/config/menus.ts
+++ src/config/menus.ts
@@ -38,6 +38,7 @@ export default {
         'indent',
         'list',
         'lineHeight',
+        'table',
     ],
 
     fontNames: [
--- src/text/index.ts
+++ src/text/index.ts
@@ -25,6 +25,7 @@ type TextEventHooks = {
     toolbarClickEvents: Function[] // 菜单栏被点击
     imgClickEvents: Function[] // 图片被点击事件
     imgDragBarMouseDownEvents: Function[] //图片拖拽MouseDown
+    formClickEvents: Function[] //表格点击
 }
 
 class Text {
@@ -49,6 +50,7 @@ class Text {
             toolbarClickEvents: [],
             imgClickEvents: [],
             imgDragBarMouseDownEvents: [],
+            formClickEvents: [],
         }
     }
 
@@ -339,15 +341,46 @@ class Text {
 
         //mousedown事件
         $(document).on('mousedown', (e: Event) => {
-            e = e || event
             e.stopPropagation()
-            e.preventDefault()
 
-            const $target = e.target as HTMLElement
-            if ($target.getAttribute('class') === 'w-e-img-drag-rb') {
+            const target = e.target as HTMLElement
+            const $target = $(target)
+            if ($target.hasClass('w-e-img-drag-rb')) {
+                // 点击的元素，是图片拖拽调整大小的 bar
                 const imgDragBarMouseDownEvents = eventHooks.imgDragBarMouseDownEvents
                 imgDragBarMouseDownEvents.forEach(fn => fn())
             }
+        })
+
+        //table cilik
+        $textElem.on('click', (e: Event) => {
+            e.preventDefault()
+
+            // 存储元素
+            let $dom: DomElement | null = null
+
+            const target = e.target as HTMLElement
+            const $target = $(target)
+
+            //点击获取的元素 是td th
+            if ($target.getNodeName() === 'TD' || $target.getNodeName() === 'TH') {
+                $dom = $(getParentNode($target.elems[0], editor))
+            }
+
+            //获取当前table的元素
+            function getParentNode($node: HTMLElement, editor: Editor): HTMLElement {
+                const $parent = $($node.parentNode)
+                if (editor.$textElem.equal($parent)) {
+                    return $node
+                } else {
+                    return getParentNode($parent.elems[0], editor)
+                }
+            }
+
+            if ($dom == null) return // 没有table范围内，则返回
+
+            const formClickEvents = eventHooks.formClickEvents
+            formClickEvents.forEach(fn => fn($dom))
         })
     }
 }
