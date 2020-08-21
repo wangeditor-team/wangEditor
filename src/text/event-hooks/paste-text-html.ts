b3aeef6e86720f08fa3df7f0322dbcacf329ec76
q378532364 <378532364@qq.com>
2020-08-21 22:18:52 +0800

    fix:修复粘贴光标bug

--- src/text/event-hooks/paste-text-html.ts
+++ src/text/event-hooks/paste-text-html.ts
@@ -6,12 +6,26 @@
 import Editor from '../../editor/index'
 import { getPasteText, getPasteHtml } from '../paste/paste-event'
 import { isFunction } from '../../utils/util'
+import $ from '../../utils/dom-core'
 
 /**
  * 粘贴文本和 html
  * @param editor 编辑器对象
  * @param pasteEvents 粘贴事件列表
  */
+
+function _foramthtml(val: string) {
+    let pasteText = val
+    pasteText = pasteText.replace(/<br>|<br\/>/gim, '')
+
+    // div 全部替换为 p 标签
+    pasteText = pasteText.replace(/<div>/gim, '<p>').replace(/<\/div>/gim, '</p>')
+
+    // 不允许空行，放在最后
+    pasteText = pasteText.replace(/<p><\/p>/gim, '<p><br></p>')
+    return pasteText
+}
+
 function pasteTextHtml(editor: Editor, pasteEvents: Function[]) {
     function fn(e: Event) {
         // 获取配置
@@ -25,8 +39,13 @@ function pasteTextHtml(editor: Editor, pasteEvents: Function[]) {
         let pasteText = getPasteText(e as ClipboardEvent)
         pasteText = pasteText.replace(/\n/gm, '<br>')
 
+        // 获取选取
+        const _range = editor.selection.getRange()
         // 当前选区所在的 DOM 节点
         const $selectionElem = editor.selection.getSelectionContainerElem()
+        // 获取end的偏移位置
+        //   var end = _range?.endOffset
+
         if (!$selectionElem) {
             return
         }
@@ -38,7 +57,7 @@ function pasteTextHtml(editor: Editor, pasteEvents: Function[]) {
                 // 用户自定义过滤处理粘贴内容
                 pasteText = '' + (pasteTextHandle(pasteText) || '')
             }
-            editor.cmd.do('insertHTML', `<p>${pasteText}</p>`)
+            editor.cmd.do('insertHTML', `<p>${_foramthtml(pasteText)}</p>`)
             return
         }
 
@@ -55,17 +74,22 @@ function pasteTextHtml(editor: Editor, pasteEvents: Function[]) {
                 // 用户自定义过滤处理粘贴内容
                 pasteHtml = '' + (pasteTextHandle(pasteHtml) || '') // html
             }
-            editor.cmd.do('insertHTML', pasteHtml)
+            editor.cmd.do('insertHTML', _foramthtml(pasteHtml))
         } catch (ex) {
             // 此时使用 pasteText 来兼容一下
             if (pasteTextHandle && isFunction(pasteTextHandle)) {
                 // 用户自定义过滤处理粘贴内容
                 pasteText = '' + (pasteTextHandle(pasteText) || '')
             }
-            editor.cmd.do('insertHTML', `<p>${pasteText}</p>`) // text
+            editor.cmd.do('insertHTML', `<p>${_foramthtml(pasteText)}</p>`) // text
         } finally {
             // 粘贴之后，统一进行格式化
-            editor.txt.formatHtml()
+            //   editor.txt.formatHtml()
+            // 忽略 <br> 换行
+            // if (end) {
+            //     _range?.setStart($selectionElem.elems[0], end)
+            //     _range?.setEnd($selectionElem.elems[0], end)
+            // }
         }
     }
     pasteEvents.push(fn)
