158e896506b8b1673c9d199cbef9b77276b322b5
wangfupeng <wangfupeng1988@163.com>
2020-09-03 16:35:27 +0800

    fix: windows 上传图片错误

--- server/controller/save-file.js
+++ server/controller/save-file.js
@@ -3,10 +3,14 @@
  * @author wangfupeng
  */
 
+const os = require('os')
 const fs = require('fs')
 const path = require('path')
 const formidable = require('formidable')
 const { objForEach } = require('../util')
+const FILE_FOLDER = 'upload-files'
+const isWindows = os.type().toLowerCase().indexOf('windows') >= 0
+const TMP_FOLDER = 'upload-files-tmp'
 
 /**
  * 保存上传的文件
@@ -16,12 +20,22 @@ function saveFiles(req) {
     return new Promise((resolve, reject) => {
         const imgLinks = []
         const form = formidable({ multiples: true })
+
+        // windows 系统，处理 rename 报错
+        if (isWindows) {
+            const tmpPath = path.resolve(__dirname, TMP_FOLDER)
+            if (!fs.existsSync(tmpPath)) {
+                fs.mkdirSync(tmpPath)
+            }
+            form.uploadDir = TMP_FOLDER
+        }
+
         form.parse(req, function (err, fields, files) {
             if (err) {
                 reject('formidable, form.parse err', err.stack)
             }
             // 存储图片的文件夹
-            const storePath = path.resolve(__dirname, '..', 'upload-files')
+            const storePath = path.resolve(__dirname, '..', FILE_FOLDER)
             if (!fs.existsSync(storePath)) {
                 fs.mkdirSync(storePath)
             }
@@ -36,7 +50,7 @@ function saveFiles(req) {
                 // 将临时文件保存为正式文件
                 fs.renameSync(tempFilePath, fullFileName)
                 // 存储链接
-                imgLinks.push('/server/upload-files/' + fileName)
+                imgLinks.push(`/server/${FILE_FOLDER}/` + fileName)
             })
 
             // 返回结果
