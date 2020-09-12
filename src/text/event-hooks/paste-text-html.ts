60edb18244c654ab98227ab92de7fbb5bb4b7757
q378532364 <378532364@qq.com>
2020-09-12 09:45:27 +0800

    fix：修复设置标题回车后粘贴报错的bug

--- src/text/event-hooks/paste-text-html.ts
+++ src/text/event-hooks/paste-text-html.ts
@@ -59,10 +59,15 @@ function pasteTextHtml(editor: Editor, pasteEvents: Function[]) {
         if (!$selectionElem) {
             return
         }
-        const nodeName = $selectionElem.getNodeName()
-        const $topElem = $selectionElem?.getNodeTop(editor)
-        const topNodeName = $topElem?.getNodeName()
 
+        const nodeName = $selectionElem?.getNodeName()
+        const $topElem = $selectionElem?.getNodeTop(editor)
+
+        // 当前节点顶级可能没有
+        let topNodeName: string = ''
+        if ($topElem.elems[0]) {
+            topNodeName = $topElem?.getNodeName()
+        }
         // code 中只能粘贴纯文本
         if (nodeName === 'CODE' || topNodeName === 'PRE') {
             if (pasteTextHandle && isFunction(pasteTextHandle)) {
