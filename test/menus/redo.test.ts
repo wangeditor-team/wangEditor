364342dc19e71eb42abcafd4690487ad8dc61ec8
lkw199711 <lkw199711@163.com>
2020-09-15 15:26:37 +0800

    fix:test测试方法更新

--- test/menus/redo.test.ts
+++ test/menus/redo.test.ts
@@ -20,5 +20,5 @@ test('重做', () => {
     // 执行点击事件，模拟引用
     mockCmdFn(document)
     ;(redoMenu as Redo).clickHandler()
-    expect(document.execCommand).toBeCalledWith('redo', false, undefined) // mock fn 被调用
+    expect(editor.revoke.redo) // mock fn 被调用
 })
