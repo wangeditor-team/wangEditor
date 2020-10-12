71277572411b7c4fe6b5a766bbf553334d916131
UOrb <798008773@qq.com>
2020-10-12 22:39:32 +0800

    feat: server 输出url地址 package 添加 run start

--- server/www.js
+++ server/www.js
@@ -83,4 +83,5 @@ function onListening() {
     var addr = server.address()
     var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
     debug('Listening on ' + bind)
+    console.log(`http://127.0.0.1:${port}/examples/index.html`)
 }
