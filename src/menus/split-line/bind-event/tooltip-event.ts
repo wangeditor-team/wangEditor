85cbd7a955a9c4a09586d031e79e1501361ccacf
clinfc <1962109568@qq.com>
2020-10-21 19:49:30 +0800

    fix：多编辑器实例情况下tooltip功能异常

--- src/menus/split-line/bind-event/tooltip-event.ts
+++ src/menus/split-line/bind-event/tooltip-event.ts
@@ -6,48 +6,56 @@ import $, { DomElement } from '../../../utils/dom-core'
 import Tooltip, { TooltipConfType } from '../../menu-constructors/Tooltip'
 import Editor from '../../../editor/index'
 
-let tooltip: Tooltip | null
-let _editor: Editor
-
 /**
- * 显示分割线的 tooltip
- * @param $splitLine 分割线元素
+ * 生成 Tooltip 的显示隐藏函数
  */
-function showSplitLineTooltip($splitLine: DomElement): void {
-    // 定义 splitLine tooltip 配置
-    const conf: TooltipConfType = [
-        {
-            $elem: $(`<span>${_editor.i18next.t('menus.panelMenus.删除')}</span>`),
-            onClick: (editor: Editor, $splitLine: DomElement) => {
-                // 选中 分割线 元素
-                editor.selection.createRangeByElem($splitLine)
-                editor.selection.restoreSelection()
-                editor.cmd.do('delete')
+function createShowHideFn(editor: Editor) {
+    let tooltip: Tooltip | null
 
-                // 返回 true，表示执行完之后，隐藏 tooltip。否则不隐藏。
-                return true
+    /**
+     * 显示分割线的 tooltip
+     * @param $splitLine 分割线元素
+     */
+    function showSplitLineTooltip($splitLine: DomElement): void {
+        // 定义 splitLine tooltip 配置
+        const conf: TooltipConfType = [
+            {
+                $elem: $(`<span>${editor.i18next.t('menus.panelMenus.删除')}</span>`),
+                onClick: (editor: Editor, $splitLine: DomElement) => {
+                    // 选中 分割线 元素
+                    editor.selection.createRangeByElem($splitLine)
+                    editor.selection.restoreSelection()
+                    editor.cmd.do('delete')
+
+                    // 返回 true，表示执行完之后，隐藏 tooltip。否则不隐藏。
+                    return true
+                },
             },
-        },
-    ]
-    // 实例化 tooltip
-    tooltip = new Tooltip(_editor, $splitLine, conf)
-    // 创建 tooltip
-    tooltip.create()
-}
-/**
- * 隐藏分割线的 tooltip
- */
-function hideSplitLineTooltip(): void {
-    // 将 tooltip 移除
+        ]
+        // 实例化 tooltip
+        tooltip = new Tooltip(editor, $splitLine, conf)
+        // 创建 tooltip
+        tooltip.create()
+    }
 
-    if (tooltip) {
-        tooltip.remove()
-        tooltip = null
+    /**
+     * 隐藏分割线的 tooltip
+     */
+    function hideSplitLineTooltip(): void {
+        if (tooltip) {
+            tooltip.remove()
+            tooltip = null
+        }
+    }
+
+    return {
+        showSplitLineTooltip,
+        hideSplitLineTooltip,
     }
 }
 
-function bindTooltipEvent(editor: Editor): void {
-    _editor = editor
+export default function bindTooltipEvent(editor: Editor): void {
+    const { showSplitLineTooltip, hideSplitLineTooltip } = createShowHideFn(editor)
 
     // 点击分割线时，显示 tooltip
     editor.txt.eventHooks.splitLineEvents.push(showSplitLineTooltip)
@@ -59,4 +67,3 @@ function bindTooltipEvent(editor: Editor): void {
     editor.txt.eventHooks.menuClickEvents.push(hideSplitLineTooltip)
     editor.txt.eventHooks.textScrollEvents.push(hideSplitLineTooltip)
 }
-export default bindTooltipEvent
