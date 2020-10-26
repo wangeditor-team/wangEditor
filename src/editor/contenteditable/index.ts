e3ff913fffd420a3cb907f515e207b39ef3d5e1f
lichunlin0618 <5248972+lichunlin_0618@user.noreply.gitee.com>
2020-10-26 22:43:31 +0800

    fix: 修复重复创建幕布，切换抖动

--- src/editor/contenteditable/index.ts
+++ src/editor/contenteditable/index.ts
@@ -8,25 +8,33 @@ import '../../assets/style/contenteditable.less'
 import ZIndex from '../z-index'
 
 const zindex = new ZIndex()
+let isCurtain = false // 避免重复生成幕布
 
 // 创建幕布
 function createCurtain(editor: Editor) {
-    zindex.init(editor)
-    // 隐藏编辑区域
-    editor.$textElem.hide()
-    // 生成div 渲染编辑内容
-    let menuZindexValue = zindex.get('menu')
-    const content = editor.txt.html()
-    const $contentDom = $(
-        `<div class="w-e-content-edit-able" style="z-index:${menuZindexValue}"><div class="w-e-content-preview">${content}</div></div>`
-    )
-    editor.$textContainerElem.append($contentDom)
-    // 生成div 菜单膜布
-    let textContainerZindexValue = zindex.get('textContainer')
-    const $menuDom = $(
-        `<div class="w-e-menue-edit-able" style="z-index:${textContainerZindexValue}"></div>`
-    )
-    editor.$toolbarElem.append($menuDom)
+    if (!isCurtain) {
+        zindex.init(editor)
+        // 隐藏编辑区域
+        editor.$textElem.hide()
+        // 生成div 渲染编辑内容
+        let menuZindexValue = zindex.get('menu')
+        const content = editor.txt.html()
+        const $contentDom = $(
+            `<div class="w-e-content-edit-able" style="z-index:${menuZindexValue}">
+                <div class="w-e-content-preview w-e-text">${content}</div>
+            </div>`
+        )
+        editor.$textContainerElem.append($contentDom)
+        // 生成div 菜单膜布
+        let textContainerZindexValue = zindex.get('textContainer')
+        const $menuDom = $(
+            `<div class="w-e-menue-edit-able" style="z-index:${textContainerZindexValue}"></div>`
+        )
+        editor.$toolbarElem.append($menuDom)
+        isCurtain = true
+        return
+    }
+    console.log('已创建')
 }
 
 // 销毁幕布并显示可编辑区域
@@ -36,6 +44,7 @@ function deleteCurtain(editor: Editor) {
     const $menuDom = editor.$toolbarElem.find('.w-e-menue-edit-able')
     $menuDom.remove()
     editor.$textElem.show()
+    isCurtain = false
 }
 
 export default {
