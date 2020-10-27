5620e1ee48f8b132a62af444eda8ee30f4d24ff8
hahaaha <390519764@qq.com>
2020-10-27 13:34:32 +0800

    fix: 修复粘贴多出空行问题

--- src/text/event-hooks/paste-text-html.ts
+++ src/text/event-hooks/paste-text-html.ts
@@ -15,8 +15,6 @@ import { urlRegex } from '../../utils/const'
  */
 function formatHtml(val: string) {
     let pasteText = val
-    // 将br替换为<p><br/></p>
-    pasteText = pasteText.replace(/<br>|<br\/>/gim, '<p><br/></p>')
     // div 全部替换为 p 标签
     pasteText = pasteText.replace(/<div>/gim, '<p>').replace(/<\/div>/gim, '</p>')
     // 不允许空行，放在最后
