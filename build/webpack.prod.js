a3aa92d75f188ca2334f0e85fc136dbb0b7bc995
wangfupeng <wangfupeng1988@163.com>
2020-11-04 10:18:25 +0800

    chore: webpack analyzer

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -7,7 +7,16 @@
 const { smart } = require('webpack-merge')
 const CommonConf = require('./webpack.common')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const { distPath } = require('./myPath')
+
+// 包体积分析
+const isAnalyzer = process.env.NODE_ENV === 'production_analyzer'
+
+const plugins = [new CleanWebpackPlugin()]
+if (isAnalyzer) {
+    plugins.push(new BundleAnalyzerPlugin())
+}
 
 module.exports = smart(CommonConf, {
     mode: 'production',
@@ -18,6 +27,6 @@ module.exports = smart(CommonConf, {
         libraryTarget: 'umd',
         libraryExport: 'default',
     },
-    plugins: [new CleanWebpackPlugin()],
+    plugins,
     devtool: 'source-map',
 })
