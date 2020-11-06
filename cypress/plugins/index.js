f092e236c101fb3360c1cec0f9a0382a9e5d3655
luochao <651644100@qq.com>
2020-11-06 09:03:43 +0800

    test: 添加了获取测试覆盖率的配置

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -18,4 +18,6 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  require('@cypress/code-coverage/task')(on, config)
+  return config
 }
