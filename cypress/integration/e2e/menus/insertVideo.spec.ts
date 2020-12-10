598b3421471f385a2b97b9696e483072895fd98e
luochao <651644100@qq.com>
2020-12-10 11:30:05 +0800

    test: 优化了插入视频的测试

--- cypress/integration/e2e/menus/insertVideo.spec.ts
+++ cypress/integration/e2e/menus/insertVideo.spec.ts
@@ -36,11 +36,10 @@ describe('插入视频', () => {
         cy.get('@imgMenu').find('.w-e-panel-container').as('Panel').find('input').type(videoEl)
         cy.get('@Panel').find('.w-e-button-container button').click()
 
-        cy.get('@Editable')
-            .find('video', { timeout: 20000 })
+        cy.get('.w-e-text-container video', { timeout: 20000 })
             .should('be.visible')
             .then($video => {
-                const video = $video.get(0)
+                const video = $video.get(0) as HTMLVideoElement
                 expect(video.src).to.eq(videoUrl)
             })
     })
