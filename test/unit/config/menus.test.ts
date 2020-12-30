cf83034217fbba977d3f157042805e5abb64b3e4
rsl140 <rsl140@qq.com>
2020-12-30 23:03:14 +0800

    test: 菜单数量测试用例修改

--- test/unit/config/menus.test.ts
+++ test/unit/config/menus.test.ts
@@ -9,7 +9,7 @@ test('菜单数量', () => {
     const editor = createEditor(document, 'div1')
     const menus = editor.config.menus
     const $toolbarChildren = editor.$toolbarElem.childNodes() || []
-    let childrenLen = $toolbarChildren.length
+    let childrenLen = $toolbarChildren.length - 1
     if (editor.config.showFullScreen) childrenLen = childrenLen - 1
     // config.menus 数量，等于 toolbar 菜单的数量
     expect(menus.length).toBe(childrenLen)
