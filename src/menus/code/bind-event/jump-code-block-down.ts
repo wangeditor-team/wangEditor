e54705810f41c51e1f3c2db10dd63165e308a908
Liu <528851597@qq.com>
2021-03-09 21:56:19 +0800

    fix: 修复代码块处于最后一行时,无法输入内容的问题

--- src/menus/code/bind-event/jump-code-block-down.ts
+++ src/menus/code/bind-event/jump-code-block-down.ts
@@ -24,4 +24,32 @@ export default function bindEventJumpCodeBlock(editor: Editor) {
             $textElem.append($emptyP)
         }
     })
+    // fix: 始终保留code区域后面的空行，防止发生代码块作为最后一行无法输入的问题
+    keydownEvents.push(function (e) {
+        // 实时保存选区
+        editor.selection.saveRange()
+        // 获取当前选区的节点
+        const $selectionContainerElem = selection.getSelectionContainerElem()
+        if ($selectionContainerElem) {
+            // 获取当前选区节点的最外层元素
+            const $topElem = $selectionContainerElem.getNodeTop(editor)
+            // 获取当前选区节点的上一个元素
+            // 用来判断是否为pre标签
+            const $preElem = $topElem.length
+                ? $topElem.prev().length
+                    ? $topElem.prev()
+                    : null
+                : null
+            // 上一个元素存在，并且是pre标签，阻止删除的默认键盘事件
+            if ($preElem && $preElem.getNodeName() === 'PRE') {
+                // 光标处于选区开头（允许用户删除当前节点中已经存在的文字)
+                if (selection.getCursorPos() === 0) {
+                    // 按下delete键阻止默认行为
+                    if (e.keyCode === 8) {
+                        e.preventDefault()
+                    }
+                }
+            }
+        }
+    })
 }
--- src/menus/table/bind-event/keyboard.ts
+++ src/menus/table/bind-event/keyboard.ts
@@ -14,12 +14,13 @@ export default function bindEventKeyboardEvent(editor: Editor) {
         const $selectionContainerElem = selection.getSelectionContainerElem()
         if ($selectionContainerElem) {
             const $topElem = $selectionContainerElem.getNodeTop(editor)
-            const $preElem = $topElem.length
-                ? $topElem.prev().length
-                    ? $topElem.prev()
-                    : null
-                : null
-            if ($preElem && $preElem.getNodeName() === 'TABLE') {
+            // 获取选区所在节点的上一元素
+            const $preElem = $topElem?.prev()
+            // 判断该元素后面是否还存在元素
+            // 如果存在则允许删除
+            const $nextElem = $topElem?.getNextSibling()
+
+            if ($preElem.length && $preElem?.getNodeName() === 'TABLE' && $nextElem.length === 0) {
                 // 光标处于选区开头
                 if (selection.getCursorPos() === 0) {
                     // 按下delete键阻止默认行为
