60e6efc3b3d6c31c4834e3b40e02fc8bc4ceaea6
王福朋 <wangfupeng1988@163.com>
2021-06-24 16:22:26 +0800

    fix: beforeinput support

--- packages/core/src/text-area/event-handlers/keypress.ts
+++ packages/core/src/text-area/event-handlers/keypress.ts
@@ -9,11 +9,13 @@ import TextArea from '../TextArea'
 import { HAS_BEFORE_INPUT_SUPPORT } from '../../utils/ua'
 import { hasEditableTarget } from '../helpers'
 
-function handleKeypress(event: Event, textarea: TextArea, editor: IDomEditor) {
-  const { editorConfig } = textarea
+// 【注意】虽然 keypress 事件已经过时（建议用 keydown 取代），但这里是为了兼容 beforeinput ，所以不会在高级浏览器生效，不用升级 keydown
 
+function handleKeypress(event: Event, textarea: TextArea, editor: IDomEditor) {
   // 这里是兼容不完全支持 beforeInput 的浏览器。对于支持 beforeInput 的浏览器，会用 beforeinput 事件处理
   if (HAS_BEFORE_INPUT_SUPPORT) return
+
+  const { editorConfig } = textarea
   if (editorConfig.readOnly) return
   if (!hasEditableTarget(editor, event.target)) return
 
--- packages/core/src/utils/ua.ts
+++ packages/core/src/utils/ua.ts
@@ -26,6 +26,5 @@ export const IS_CHROME_LEGACY =
   typeof navigator !== 'undefined' &&
   /Chrome?\/(?:[0-7][0-5]|[0-6][0-9])/i.test(navigator.userAgent)
 
-// COMPAT: Firefox/Edge Legacy don't support the `beforeinput` event
-// Chrome Legacy doesn't support `beforeinput` correctly
-export const HAS_BEFORE_INPUT_SUPPORT = !(IS_FIREFOX || IS_EDGE_LEGACY || IS_CHROME_LEGACY)
+// @ts-ignore 判断浏览器是否支持 beforeinput 事件 https://www.caniuse.com/?search=beforeinput
+export const HAS_BEFORE_INPUT_SUPPORT = typeof InputEvent.prototype.getTargetRanges === 'function'
