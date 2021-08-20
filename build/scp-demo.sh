94d8fc0b35b13024b089b4585f6254d661ddc0bc
王福朋 <wangfupeng1988@163.com>
2021-08-20 09:20:55 +0800

    chore: deploy examples

--- build/scp-demo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## 拷贝 demo 到服务器
-
-localPath="./packages/editor"
-serverPath="work@106.12.198.214:~/wangEditor-team/v5-examples/editor"
-scp -r $localPath/dist $serverPath
-scp -r $localPath/examples $serverPath
