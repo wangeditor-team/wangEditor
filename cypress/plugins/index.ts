1d7e9dc2505b1fde9bf83ff6bbdd25f644c9b80f
echoLC <651644100@qq.com>
2021-10-24 15:18:40 +0800

    test: 添加了 cypress e2e 到 ci 流程

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -15,8 +15,9 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
+export default (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  // codeCoverageTask(on, config)
+  return config
 }
