8c7dc8b8efe1705af9989b040b04e2f98932cb77
王福朋 <wangfupeng1988@163.com>
2021-12-03 17:40:47 +0800

    fix: insertHtml - maxLength

--- packages/core/src/editor/plugins/with-event-data.ts
+++ packages/core/src/editor/plugins/with-event-data.ts
@@ -114,6 +114,10 @@ export const withEventData = <T extends Editor>(editor: T) => {
     const rtf = data.getData('text/rtf')
 
     if (html && !rtf) {
+      if (DomEditor.checkMaxLength(e)) {
+        return
+      }
+
       e.dangerouslyInsertHtml(html)
       return
     }
