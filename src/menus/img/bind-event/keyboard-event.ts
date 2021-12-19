2d060d5fb16ee1edf281383acf99299c0971f47c
qianfengg <fred_gqf0221@163.com>
2021-12-19 19:41:16 +0800

    fix: delete text will delete img too

--- src/menus/img/bind-event/keyboard-event.ts
+++ src/menus/img/bind-event/keyboard-event.ts
@@ -51,5 +51,13 @@ export default function bindEventKeyboardEvent(editor: Editor) {
             lastChildNodeInPrevNode =
                 lastChildNodeInPrevNode.childNodes[lastChildNodeInPrevNode.childNodes.length - 1]
         }
+
+        if (
+            lastChildNodeInPrevNode instanceof HTMLElement &&
+            lastChildNodeInPrevNode.tagName === 'IMG'
+        ) {
+            lastChildNodeInPrevNode.remove()
+            e.preventDefault()
+        }
     })
 }
