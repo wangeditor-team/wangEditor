03310eedc40283c2e228281f1f7eeea9c06c8fa9
Gavin <1490259042@qq.com>
2022-01-25 23:47:03 +0800

    fix: url或者常规文本中HTML转义字符显示异常

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -6,7 +6,7 @@
 export function EMPTY_FN() {}
 
 //用于校验是否为url格式字符串
-export const urlRegex = /^(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-.,@?^=%&amp;:/~+#]*[\w\-@?^=%&amp;/~+#])?/
+export const urlRegex = /(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-.,@?^=%&amp;:/~+#]*[\w\-@?^=%&amp;/~+#])?/g
 
 // 编辑器为了方便继续输入/换行等原因 主动生成的空标签
 export const EMPTY_P = '<p data-we-empty-p=""><br></p>'
