af1f523983f2bc4b7eaf9726d4b8a35227ab27dc
王福朋 <wangfupeng1988@163.com>
2022-02-07 18:08:27 +0800

    fix: style-to-html - 输入 a 会删除外部的 <a> 标签

--- packages/basic-modules/src/modules/text-style/style-to-html.ts
+++ packages/basic-modules/src/modules/text-style/style-to-html.ts
@@ -5,7 +5,7 @@
 
 import { Text, Descendant } from 'slate'
 import { StyledText } from './custom-types'
-import $, { getOuterHTML, getTagName } from '../../utils/dom'
+import $, { getOuterHTML, getTagName, isPlainText } from '../../utils/dom'
 
 //【注意】color bgColor fontSize fontFamily 在另外的菜单
 
@@ -36,18 +36,19 @@ function genStyledHtml(textNode: Descendant, html: string): string {
 export function styleToHtml(textNode: Descendant, textHtml: string): string {
   if (!Text.isText(textNode)) return textHtml
 
-  let $text = $()
-  try {
-    $text = $(textHtml) // textHtml 可能是 html tag ，也可能是纯字符串，所以要 try
-  } catch (err) {} // eslint-disable-line
-
-  // textHtml 是纯文本，或是 <br>，不是 html tag
-  const tagName = getTagName($text)
-  if (tagName === 'br' || $text.length === 0) {
+  if (isPlainText(textHtml)) {
+    // textHtml 是纯文本，而不是 html tag
     return genStyledHtml(textNode, textHtml)
   }
 
-  // textHtml 是 html tag ，如 <span>
+  // textHtml 是 html tag
+  const $text = $(textHtml)
+  const tagName = getTagName($text)
+
+  if (tagName === 'br') {
+    return genStyledHtml(textNode, '<br>')
+  }
+
   let innerHtml = $text.html()
   innerHtml = genStyledHtml(textNode, innerHtml)
   $text.html(innerHtml)
