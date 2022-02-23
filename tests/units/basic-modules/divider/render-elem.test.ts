d20d9527ef1ebb4b336b92005d74f817a635b6eb
王福朋 <wangfupeng1988@163.com>
2022-02-23 13:55:10 +0800

    fix: test

--- tests/units/basic-modules/divider/render-elem.test.ts
+++ tests/units/basic-modules/divider/render-elem.test.ts
@@ -17,8 +17,8 @@ describe('divider - render elem test', () => {
     const elem = { type: 'divider', children: [{ text: '' }] }
     const vnode1 = renderDividerConf.renderElem(elem, null, editor) as any
     expect(vnode1.sel).toBe('div')
-    expect(vnode1.data.className).toBe('w-e-textarea-divider')
-    expect(vnode1.data['data-selected']).toBe('') // 未选中
+    expect(vnode1.data.props.className).toBe('w-e-textarea-divider')
+    expect(vnode1.data.dataset.selected).toBe('') // 未选中
     expect(vnode1.children[0].sel).toBe('hr')
 
     editor.select(startLocation)
@@ -28,6 +28,6 @@ describe('divider - render elem test', () => {
       offset: 0,
     })
     const vnode2 = renderDividerConf.renderElem(elem, null, editor) as any
-    expect(vnode2.data['data-selected']).toBe('true') // 选中
+    expect(vnode2.data.dataset.selected).toBe('true') // 选中
   })
 })
