370d8ff5f3ec1fa931cf98f8228847a3966effaa
luochao <luochao@joyy.com>
2022-03-05 15:47:21 +0800

    test: 完善了上传图片菜单的单元测试

--- tests/setup/index.ts
+++ tests/setup/index.ts
@@ -7,3 +7,17 @@ global.crypto = {
     return nodeCrypto.randomFillSync(buffer)
   },
 }
+
+// Jest environment not contains DataTransfer object, so mock a DataTransfer class
+// @ts-ignore
+global.DataTransfer = class DataTransfer {
+  clearData() {}
+  getData(type: string) {
+    if (type === 'text/plain') return ''
+    return []
+  }
+  setData() {}
+  get files() {
+    return [new File(['124'], 'test.jpg')]
+  }
+}
